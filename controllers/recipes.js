6a1f4dc77a67a77ca04ca32b14a57858a48465bc
unknown <kassio_29@hotmail.com>
2020-02-03 00:52:40 -0300

    Initial Commit

---
+++ controllers/recipes.js
@@ -0,0 +1,125 @@
+const data = require('../data.json')
+const fs = require('fs')
+
+
+//INDEX
+exports.home = function(req, res){
+    return res.redirect("admin/recipes")
+}
+
+//HOME
+exports.index = function(req, res){
+    //return res.send('Teste')
+    return res.render("admin/recipes", { recipes: data.recipes })
+}   
+
+//CREATE
+exports.create = function(req, res){
+    return res.render("admin/create")
+}
+
+//SHOW
+exports.show = function(req, res){
+    
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find(function(recipe){
+        return id == recipe.id
+    })
+
+        if(!foundRecipe) return res.send('Recipe not found')
+        
+        return res.render ("admin/show", { recipes: foundRecipe })
+
+}
+
+//POST
+exports.post = function(req, res){
+    const keys = Object.keys(req.body)
+    
+    for(key of keys){
+        if(req.body[key] === "") return res.send ("Please, fill all fields")
+    }
+
+    let { recipe_image, recipe_ingredient, recipe_preparation, recipe_information } = req.body
+
+    
+
+    data.recipes.push({
+        id: Number(data.recipes.length + 1),
+        image: recipe_image,
+        ingredients: recipe_ingredient, 
+        preparation: recipe_preparation,
+        information: recipe_information
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){ 
+        if (err) return res.send("Write file error")
+
+        return res.redirect("admin/recipes")
+    })
+}
+
+//EDIT
+exports.edit = function(req, res){
+
+    const { id } = req.params
+
+    const foundRecipe = data.recipes.find(function(recipe){
+        return id == recipe.id
+    })
+
+    if(!foundRecipe) return res.send('Recipe not found')
+
+    const recipes = {
+        id: Number(id),
+        ...foundRecipe
+    }
+    return res.render("admin/edit", { recipes })
+}
+
+//PUT
+exports.put = function(req, res){
+    const { id, recipe_image, recipe_ingredient, recipe_preparation, recipe_information } = req.body
+    
+    const foundRecipe = data.recipes.find(function(recipe){
+        return id == recipe.id
+    })
+
+    if (!foundRecipe) return res.send ("Recipe not found!")
+
+    const recipe = {
+        ...foundRecipe,
+        ...req.body,
+        image: recipe_image,
+        ingredients: recipe_ingredient, 
+        preparation: recipe_preparation,
+        information: recipe_information
+    }
+
+    data.recipes[id - 1] = recipe
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("write error")
+        return res.redirect(`/admin/recipes/${id}`)
+    })
+
+}
+
+//DELETE
+exports.delete = function(req, res){
+
+    const { id } = req.body
+
+    const filteredRecipes = data.recipes.filter(function(recipe){
+        return recipe.id != id
+    })
+
+    data.recipes = filteredRecipes
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error")
+
+        return res.redirect("recipes")
+    })
+}
---
+++ public/script.js
@@ -0,0 +1,79 @@
+// Lógica para lista de receitas, quando uma for clicada, será direcionado para o front de detalhes da receita (recipes.njk)
+const cards = document.querySelectorAll('.view-recipe')
+
+for(let card of cards){
+    card.addEventListener('click', function(){
+        const recipeId = card.getAttribute('id')
+        window.location.href = `/admin/recipes/${recipeId}`
+    })
+}
+
+// Lógica para esconder/mostrar os detalhes da receita
+const buttons = document.querySelectorAll('h4')
+const contents = document.querySelectorAll('.topic-content')
+
+var i = 0
+
+for(let button of buttons){    
+    
+    const content = contents[i]
+    button.addEventListener('click', function(){    
+
+        if (button.innerHTML === 'ESCONDER'){
+            button.innerHTML = 'MOSTRAR'
+            content.classList.remove('show')
+            content.classList.add('hide')
+            
+        } else {
+            button.innerHTML = 'ESCONDER'
+            content.classList.remove('hide')
+            content.classList.add('show')
+            
+        }
+    })
+    i += 1
+}
+
+// Add fields ingredients on create.njk
+function addIngredient(){
+    
+    const ingredients = document.querySelector("#ingredients")
+    const fieldContainer = document.querySelectorAll(".ingredient")
+
+    //Realiza um clone do último ingrediente adicionado.
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode( true )
+
+    //Não adiciona um input se o ultimo tem um valor vazio.
+    if(newField.children[0].value == "") return false
+
+    //Deixa o valor do imput vazio
+    newField.children[0].value = ""
+    ingredients.appendChild(newField)
+
+}
+
+document
+    .querySelector(".add-ingredient")
+    .addEventListener('click', addIngredient)
+
+// Add fields preparation on create.njk
+function addPreparation(){
+
+    const preparation_itens = document.querySelector("#preparation-itens")
+    const fieldContainer = document.querySelectorAll(".preparation")
+
+    //Realiza um clone do último ingrediente adicionado.
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode( true )
+
+    //Não adiciona um input se o ultimo tem um valor vazio.
+    if(newField.children[0].value == "") return false
+
+    //Deixa o valor do imput vazio
+    newField.children[0].value = ""
+    preparation_itens.appendChild(newField)
+
+}
+
+document
+    .querySelector(".add-preparation")
+    .addEventListener('click', addPreparation)
