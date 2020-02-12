fcc88661b8acb7844da092d863086afd4fa11edc
unknown <kassio_29@hotmail.com>
2020-02-12 01:18:53 -0300

    Feature: Now the insert instruction includes de chef_id

--- src/app/controllers/recipes.js
+++ src/app/controllers/recipes.js
@@ -33,7 +33,10 @@ module.exports = {
     },
     // Admin Create - Render Admin create recipe page
     create ( req, res ) {
-        return res.render("admin/create")
+        
+        Recipe.chefsSelectOptions(function(options) {
+            return res.render("admin/create", { chefOptions: options })
+        })
     },
     // SHOW - Show the recipes details in show page
     show ( req, res ) {
